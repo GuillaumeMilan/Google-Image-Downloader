1fdedcb497a90b0e6624b6754c72ed7436479e35
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-10-09 13:25:10 +0200

    IT WORKS GUYS

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
 echo "Nous allons changer aléatoirement votre fond d'écran"
+current_dir=$(pwd)
+echo "Nous sommes dans le dossier $current_dir"
 cd ~
 rm -rf .RandomWallPaperByGuignomes
 mkdir .RandomWallPaperByGuignomes
@@ -9,6 +11,10 @@ wget -q http://www.palabrasaleatorias.com/mots-aleatoires.php?fs=1&fs2=1&Submit=
 sleep 3
 var=$(awk '/<div style="font-size:3em; color:#6200C5;">/{getline; print}' *)
 mot=$(echo $var | sed -n "s/<\/div>//p")
+echo "Votre mot est : $mot"
 rm *
-
+cd $current_dir
+python image_downloader.py --safe-mode "$mot wallpaper" > /dev/null
+echo "Nous sommes dans le dossier $current_dir"
+gsettings set org.gnome.desktop.background picture-uri file://$current_dir/download_images/Google_0008.jpeg
 exit 0
