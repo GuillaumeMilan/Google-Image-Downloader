81f654da50735509fae97a79a419f600e0dc0025
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-10-09 13:34:35 +0200

    install file

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install --user selenium 
+pip install --user futures 
+
+npm install phantomjs
+
+current_dir=$(pwd)
+echo "$PATH=$PATH:$current_dir/node_modules/phantomjs/lib/phantom/bin" >> ~/.bashrc
